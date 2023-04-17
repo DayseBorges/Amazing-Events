fcd37e5438e2903ee0a355cfc68f48256b43b86b
Dayse Borges <dayseborges030798@gmail.com>
2023-04-17 09:18:22 -0300

    update

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,10 @@
-const cardsContainer = document.getElementById('cards_container');
+const cardsContainer = document.getElementById("cards_container");
 const imprimirData = (data) => {
-    for ( let event of data.events ){
-        const cardDiv = document.createElement("div");
-        cardDiv.className = "card";  
-        cardDiv.classList.add("m-3");
-        cardDiv.innerHTML = `
+  for (let event of data.events) {
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+    cardDiv.classList.add("m-3");
+    cardDiv.innerHTML = `
             <img src="${event.image}" class="card-img-top" alt="..." />
             <div class="card-body container-relative">
                 <h5 class="card-title">${event.name}</h5>
@@ -13,22 +13,22 @@ const imprimirData = (data) => {
                 </p>
                 <a href="../pages/details.html?id=${event._id}" class="btn btn-primary btn-bottom">Ver Más</a>
             </div>
-        `
-        cardsContainer.appendChild(cardDiv)
-    }
+        `;
+    cardsContainer.appendChild(cardDiv);
+  }
 };
 
-const categoryContainer = document.getElementById('filters');
+const categoryContainer = document.getElementById("filters");
 const categorias = (data) => {
-    const categoriasUnicas = {};
-    for (let event of data.events) {
-        const categoria = event.category;
+  const categoriasUnicas = {};
+  for (let event of data.events) {
+    const categoria = event.category;
 
-        if (!categoriasUnicas[categoria]) {
-            const checkbox = document.createElement("div");
-            checkbox.className = "form-check";
-            checkbox.classList.add("m-2");
-            checkbox.innerHTML += `
+    if (!categoriasUnicas[categoria]) {
+      const checkbox = document.createElement("div");
+      checkbox.className = "form-check";
+      checkbox.classList.add("m-2");
+      checkbox.innerHTML += `
             <input
                   class="form-check-input"
                   type="checkbox"
@@ -36,42 +36,37 @@ const categorias = (data) => {
                   id="check1"
                 />
                 <label class="form-check-label" for="check1"> ${event.category} </label>
-            `
-            categoryContainer.appendChild(checkbox)
-            categoriasUnicas[categoria] = true; 
-        }
+            `;
+      categoryContainer.appendChild(checkbox);
+      categoriasUnicas[categoria] = true;
     }
-}
+  }
+};
 
-    
 const getCategorys = () => {
-    const inputs = document.querySelectorAll("input[type=checkbox]")
-    return inputs;
-}
+  const inputs = document.querySelectorAll("input[type=checkbox]");
+  return inputs;
+};
+
 
 const listenerEvent = () => {
-    let inputs = getCategorys();
-    inputs.forEach(input => {
-        input.addEventListener("change", () => {
-            input.checked ?
-                console.log("ok") :
-                console.log("no")
-            
-        })
+  let inputs = getCategorys();
+  inputs.forEach((input) => {
+    input.addEventListener("change", () => {
+      input.checked ? console.log("ok") : console.log("no");
     });
-}
+  });
+};
 
 
 fetch("https://mindhub-xj03.onrender.com/api/amazing")
-    .then(resp => {
-        return resp.json();
-    })
-        .then( data => {
-            imprimirData(data);
-            categorias(data);
-            getCategorys();
-            listenerEvent()
-        });
-
-
-        
+  .then((resp) => {
+    return resp.json();
+  })
+  .then((data) => {
+    imprimirData(data);
+    categorias(data);
+    getCategorys();
+    listenerEvent();
+    dataCategory()
+  });
